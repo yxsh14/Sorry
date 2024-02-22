f1e12c064f594d7fe77dc35b3a2098fce7c86f7e
Your Name <you@example.com>
2024-02-23 00:52:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import './App.css';
+import celebrationImage from './images/happy.png'; // Import the celebration image
+import cryingImage from './images/sad.png'; // Import the crying image
+
+function App() {
+  const [showCard, setShowCard] = useState(false);
+  const [forgiven, setForgiven] = useState(false);
+
+  const handleForgive = () => {
+    setShowCard(true);
+    setForgiven(true);
+  };
+
+  const handleNotForgive = () => {
+    setShowCard(true);
+    setForgiven(false);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Maafi-Nama</h1>
+        <h2>I'm really sorry Prachi. Won't ever happen again!</h2>
+        <h4>-Brett</h4>
+        
+        <div className="button-container">
+          <button onClick={handleForgive}>Yes</button>
+          <button onClick={handleNotForgive}>No</button>
+        </div>
+        {showCard && (
+          <div className="card">
+            <h2>{forgiven ? 'Hurray!' : 'Oh No!'}</h2>
+            <p>{forgiven ? 'Thank you for accepting my apologies. See you in college tomorrow.' : 'You have to forgive me'}</p>
+            <img src={forgiven ? celebrationImage : cryingImage} alt={forgiven ? 'Celebration' : 'Crying'} />
+          </div>
+        )}
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
